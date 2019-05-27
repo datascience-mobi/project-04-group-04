1cd0dcc17c9ade77bc6b68c9d549ee27c01dba59
Bene <benedict@wolf-bonn.com>
2019-05-27 21:33:06 +0200

    Update WWDKClustering.py

--- WWDKClustering.py
+++ WWDKClustering.py
@@ -69,30 +69,62 @@ class Kmeans():               # Input: processed dataset, Output: clustered data
                 graph = pd.DataFrame(self._data[np.argwhere(self._dist == i)].squeeze())
                 center = pd.DataFrame(self._clusters[i]).T
                 #print("Cluster"+ str(i) +  " -- Assigned Points \n" + str(graph))
-                plt.plot(graph[0], graph[1], "o")
+                plt.plot(graph[0], graph[1], "o",markersize=2)
                 plt.plot(center[0],center[1], "kx")
             plt.show
         except TypeError:
             print("Run a clustering method first!")
 class DataLoader(): # Input:dData, Output: processed dataset ready for kmeans
     
-    def __init__(self, data):
-        self._data = data
+    """Encapsulates data for clustering."""
+    def __init__(self, page):
+        self.page = tarfile.open(url.urlretrieve(page, filename=None)[0]).extractall()
+        self.source = './filtered_gene_bc_matrices/hg19/'
+        self.matrix = sc.read_10x_mtx(self.source, var_names='gene_symbols', cache=True)
+        self.data = self.page
     def url_extract(): #extracts the data from url
         pass
     
     def strip(): # Data preprocessing
         pass
-    def pca(comp1, comp2): # Principal component analysis
-        scaled = preprocessing.scale(self._data)
+    def pca(self): # Principal component analysis
+        #scaled = preprocessing.scale(self._data)
         pca = PCA()
-        pca.fit(self._data)
-        per_var = np.round(pca.explained_variance_ratio_*100, decimals= 1)
-        pca_data = pca.transform(self-_data)
-        labels = ["PC" + str(x) for x in range(1,len(per_var)+1)]
+        return pca.fit_transform(self.data)
+        #per_var = np.round(pca.explained_variance_ratio_*100, decimals= 1)
+        #pca_data = pca.transform(self-_data)
+        #labels = ["PC" + str(x) for x in range(1,len(per_var)+1)]
     def t_sne(): #t-distributed stochastic neighbor embedding
         pass
-    def show():
-        pass
+    def to_matrix(self):                                # converts to scanpy matrix 
+        return self.matrix
+        
+    def to_array(self):                              # converts data to numpy arrays
+        self.matrix.var_names_make_unique()
+        ar_data = self.matrix._X.todense().getA()
+        return ar_data
+    
+    def to_df(self):                                 # converts data to pandas dataframe
+        self.data = self.matrix.to_df()
+
+        return self.data
+        
+    def col_names(self):                              #returns column names as a list
+        columns = []
+        with open(self.source + "genes.tsv") as file:
+            reader = csv.reader(file, delimiter='\t')
+            for row in reader:
+                columns.append(row[1])
+            return columns
+
+    def row_names(self):                               #returns row names as a list
+        rows = []
+        with open(self.source + "barcodes.tsv") as file:
+            reader = csv.reader(file, delimiter='\t')
+            for row in reader:
+                rows.append(row[0]) 
+        return rows
+
+
 class Plotter(): # not sure about that one yet
     pass
