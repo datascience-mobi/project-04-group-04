e567e9e48219f30e3ced791f696d77813c376f72
Thorged <thorge_d@web.de>
2019-07-15 12:50:28 +0200

    Update Utilities.py

--- WWDK_Package/Utilities.py
+++ WWDK_Package/Utilities.py
@@ -8,15 +8,17 @@ import pandas as pd
 import time
 
 def time_k_plot(data, iterations, runs):
-    liste = []
-    sklearn_liste = []
+    liste = [0]
+    sklearn_liste = [0]
     #inet_liste = []
     for i in range(iterations):
+        meantime = []
+        sk_meantime = []
         for j in range(runs):
             
-            meantime =[]
+            
             start = time.time()
-            lib = cl.Kmeans(inits=10, method="rng", k=i+1, tol= 1)
+            lib = cl.Kmeans(inits=10, method="rng", k=i+1)
             lib.fit(data)
             end = time.time()
             meantime.append(end-start)
@@ -27,44 +29,77 @@ def time_k_plot(data, iterations, runs):
             end = time.time()
             inet_meantime.append(end-start)
             '''
-            sk_meantime = []
             start = time.time()
-            lib = sk.KMeans(init="random",n_init=i+1)
+            lib = sk.KMeans(init="random", n_init=10, n_clusters=i+1)
             lib.fit(data)
             end = time.time()
             sk_meantime.append(end-start)
+            
         liste.append(np.mean(meantime))
         sklearn_liste.append(np.mean(sk_meantime))
         #inet_liste.append(np.mean(inet_meantime))
      
     #print(lib.inertia_)
-    plt.plot(liste)
+    plt.plot(liste, label='WWDK')
     plt.plot(liste, "kx")
-    plt.plot(sklearn_liste, "r")
+    plt.plot(sklearn_liste, "r", label='sklearn')
     plt.plot(sklearn_liste, "kx")
     #plt.plot(inet_liste, "g")
     #plt.plot(inet_liste, "kx")
     plt.xlabel("k")
     plt.ylabel("time[s]")
+    plt.legend()
+    return plt.show()
+
+def inertia_k_plot(data, iterations, runs):
+    inertia = [0]
+    sklearn_inertia = [0]
+    #inet_liste = []
+    for i in range(iterations):
+        meaninertia = []
+        sk_meaninertia = []
+        for j in range(runs):
+
+            lib = cl.Kmeans(inits=10, method="rng", k=i+1)
+            lib.fit(data)
+            meaninertia.append(lib.inertia_)
+            
+            lib = sk.KMeans(init="random", n_init=10, n_clusters=i+1)
+            lib.fit(data)
+            sk_meaninertia.append(lib.inertia_)
+
+        inertia.append(np.mean(meaninertia))
+        sklearn_inertia.append(np.mean(sk_meaninertia))
+        #inet_liste.append(np.mean(inet_meantime)
+        
+    plt.plot(inertia, label='WWDK')
+    plt.plot(inertia, "kx")
+    plt.plot(sklearn_inertia, "r", label='sklearn')
+    plt.plot(sklearn_inertia, "kx")
+    plt.xlabel("k")
+    plt.ylabel("inertia")
+    plt.legend()
     return plt.show()
 
 def time_init_plot(data, iterations, runs):
-    liste = []
-    sklearn_liste = []
+    liste = [0]
+    sklearn_liste = [0]
     
     for i in range(iterations):
+        meantime =[]
+        sk_meantime = []
         for j in range(runs):
             
-            meantime =[]
+            
             start = time.time()
-            lib = cl.Kmeans(inits=i+1, method="rng", k=8,tol= 1)
+            lib = cl.Kmeans(inits=i+1, method="rng", k=8)
             lib.fit(data)
             end = time.time()
             meantime.append(end-start)
             
-            sk_meantime = []
+            
             start = time.time()
-            lib = sk.KMeans(init="random",n_init=i+1)
+            lib = sk.KMeans(init="random",n_clusters=8, n_init=i+1)
             lib.fit(data)
             end = time.time()
             sk_meantime.append(end-start)
@@ -72,12 +107,13 @@ def time_init_plot(data, iterations, runs):
         sklearn_liste.append(np.mean(sk_meantime))
      
     #print(lib.inertia_)
-    plt.plot(liste)
+    plt.plot(liste, label='WWDK')
     plt.plot(liste, "kx")
-    plt.plot(sklearn_liste, "r")
+    plt.plot(sklearn_liste, "r", label='sklearn')
     plt.plot(sklearn_liste, "kx")
     plt.xlabel("inits")
     plt.ylabel("time[s]")
+    plt.legend()
     return plt.show()
 
 def elbow_plot(data, max_k):
@@ -92,10 +128,10 @@ def elbow_plot(data, max_k):
     plt.ylabel("Sum of squared distances")
     return plt.show()
 
-def plot(data):
+def plot(data, k):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    for i in range(data._k):
+    for i in range(k):
         graph = pd.DataFrame(data._data[np.argwhere(data.labels_ == i)].squeeze())
         center = pd.DataFrame(data.cluster_centers_[i]).T
         #print("Cluster"+ str(i) +  " -- Assigned Points \n" + str(graph))
@@ -103,4 +139,17 @@ def plot(data):
         ax.plot(center[0],center[1], "kx")
         ax.annotate("Cluster " + str(i), xy = (center[0],center[1])
             )
+    return plt.show()
+
+def plot_Compare(data, dist, clusters,k):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    for i in range(k):
+        graph = pd.DataFrame(data[np.argwhere(dist == i)].squeeze())
+        center = pd.DataFrame(clusters[i]).T
+        #print("Cluster"+ str(i) +  " -- Assigned Points \n" + str(graph))
+        ax.plot(graph[0], graph[1], "o")
+        ax.plot(center[0],center[1], "kx")
+        ax.annotate("Cluster " + str(i), xy = (center[0],center[1])
+            )   
     return plt.show()
